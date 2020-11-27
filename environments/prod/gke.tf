b81fe5616928a04acc10480ccb07ca2d2c3b4f7d
kdkim2000 <kdkim2000@gmail.com>
2020-11-27 17:32:13 +0900

    gke.tf

--- environments/prod/gke.tf
+++ environments/prod/gke.tf
@@ -69,8 +69,8 @@ resource "google_container_node_pool" "primary_nodes" {
   }
  
   autoscaling {
-    min_node_count = 2
-    max_node_count = 4
+    min_node_count = 1
+    max_node_count = 2
   }
 }
  
