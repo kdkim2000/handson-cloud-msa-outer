24697ac1865a8fcfc1a5a84172bb8495e03e5573
kdkim2000 <kdkim2000@gmail.com>
2020-11-28 19:24:40 +0900

    gke.tf

--- environments/prod/gke.tf
+++ environments/prod/gke.tf
@@ -46,13 +46,13 @@ resource "google_container_node_pool" "primary_nodes" {
   name       = "${google_container_cluster.primary.name}"
   location   = var.region
   cluster    = google_container_cluster.primary.name
-  node_count = var.gke_num_nodes
 
   node_config {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
       "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/trace.append",
     ]
 
     labels = {
@@ -71,4 +71,23 @@ resource "google_container_node_pool" "primary_nodes" {
     min_node_count = 1
     max_node_count = 2
   }
+}
+
+module "gke_auth" {
+  source  = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  version = "~> 9.1"
+
+  project_id   = var.project_id
+  cluster_name = google_container_cluster.primary.name
+  location     = google_container_cluster.primary.location
+}
+
+resource "kubernetes_namespace" "eshop-app" {
+    metadata {
+        annotations      = {}
+        labels           = {
+          istio-injection = "enabled"
+        }
+        name             = "eshop-app"
+    }
 }
